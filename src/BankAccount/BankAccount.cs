ac84dad97197583cd00bfbf24a28e17decabc31a
Larry-Sentore <l.sentore@alustudent.com>
2026-06-29 10:00:20 +0200

    implemented bankaccount class

--- src/BankAccount/BankAccount.cs
+++ src/BankAccount/BankAccount.cs
@@ -1,5 +1,28 @@
-namespace BankAccount;
-
-public class BankAccount
+namespace BankAccountApp
 {
+    public class BankAccount
+    {
+        private decimal _balance;
+
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Deposit amount must be positive.");
+
+            _balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Withdrawal amount must be positive.");
+
+            if (amount > _balance)
+                throw new InvalidOperationException("Insufficient funds. Cannot overdraft.");
+
+            _balance -= amount;
+        }
+
+        public decimal GetBalance() => _balance;
+    }
 }
