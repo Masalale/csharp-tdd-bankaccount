9bef9993bbb85f4f47e9466f9225bfeaa65a7c7d
Ngash <clarenchomba59@gmail.com>
2026-06-29 10:59:33 +0200

    add unit tests for deposit and withdrawal operations in BankAccountTests

--- tests/BankAccount.Tests/BankAccountTests.cs
+++ tests/BankAccount.Tests/BankAccountTests.cs
@@ -22,26 +22,35 @@ namespace BankAccount.Tests
         [Test]
         public void Deposit_100_Balance_ShouldBe100()
         {
+            _account.Deposit(100m);
+            Assert.That(_account.GetBalance(), Is.EqualTo(100m));
         }
 
         [Test]
         public void Deposit_NegativeAmount_ShouldThrow()
         {
+            Assert.Throws<ArgumentException>(() => _account.Deposit(-1m));
         }
 
         [Test]
         public void Withdraw_50_FromAccountWith100_BalanceShouldBe50()
         {
+            _account.Deposit(100m);
+            _account.Withdraw(50m);
+            Assert.That(_account.GetBalance(), Is.EqualTo(50m));
         }
 
         [Test]
         public void Withdraw_MoreThanBalance_ShouldThrow()
         {
+            _account.Deposit(100m);
+            Assert.Throws<InvalidOperationException>(() => _account.Withdraw(200m));
         }
 
         [Test]
         public void Withdraw_NegativeAmount_ShouldThrow()
         {
+            Assert.Throws<ArgumentException>(() => _account.Withdraw(-1m));
         }
     }
 }
